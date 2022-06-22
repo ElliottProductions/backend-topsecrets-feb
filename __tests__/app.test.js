74de61084f88f68b36e3b030a72cfa51198daf0c
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-06-22 16:11:31 -0700

    new user working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,38 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const mockDuck = {
+  email: 'test@example.com',
+  password: '12345',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockDuck.password;
+
+  // Create an "agent" that gives us the ability
+  // to store cookies between requests in a test
+  const agent = request.agent(app);
+
+  // Create a user to sign in with
+  const user = await UserService.create({ ...mockDuck, ...userProps });
+
+  // ...then sign in
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('create new user using mockDuck', async () => {
+    const response = await request(app).post('/api/v1/users').send(mockDuck);
+    const { email } = mockDuck;
+
+    expect(response.body.email).toBe('test@example.com');
   });
   afterAll(() => {
     pool.end();
